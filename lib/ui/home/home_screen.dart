f9292998c0ab53b99a61ab435433007b820cc696
thefirsthero <kingvukosi@outlook.com>
2023-06-12 18:52:38 +0200

    removed unused import

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,12 +1,11 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_login_screen/constants.dart';
 import 'package:flutter_login_screen/model/user.dart';
 import 'package:flutter_login_screen/services/helper.dart';
 import 'package:flutter_login_screen/ui/auth/authentication_bloc.dart';
 import 'package:flutter_login_screen/ui/auth/welcome/welcome_screen.dart';
+
+import 'package:flutter_login_screen/ui/home/drawer/drawer_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   final User user;
@@ -35,43 +34,8 @@ class _HomeState extends State<HomeScreen> {
         }
       },
       child: Scaffold(
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              const DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Color(colorPrimary),
-                ),
-                child: Text(
-                  'Drawer Header',
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-              ListTile(
-                title: Text(
-                  'Logout',
-                  style: TextStyle(
-                      color: isDarkMode(context)
-                          ? Colors.grey.shade50
-                          : Colors.grey.shade900),
-                ),
-                leading: Transform.rotate(
-                  angle: pi / 1,
-                  child: Icon(
-                    Icons.exit_to_app,
-                    color: isDarkMode(context)
-                        ? Colors.grey.shade50
-                        : Colors.grey.shade900,
-                  ),
-                ),
-                onTap: () {
-                  context.read<AuthenticationBloc>().add(LogoutEvent());
-                },
-              ),
-            ],
-          ),
-        ),
+        drawer: DrawerWidget(user: user), // Use the DrawerWidget
+
         appBar: AppBar(
           title: Text(
             'Home',
@@ -88,43 +52,7 @@ class _HomeState extends State<HomeScreen> {
               isDarkMode(context) ? Colors.grey.shade900 : Colors.grey.shade50,
           centerTitle: true,
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              user.profilePictureURL == ''
-                  ? CircleAvatar(
-                      radius: 35,
-                      backgroundColor: Colors.grey.shade400,
-                      child: ClipOval(
-                        child: SizedBox(
-                          width: 70,
-                          height: 70,
-                          child: Image.asset(
-                            'assets/images/placeholder.jpg',
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                    )
-                  : displayCircleImage(user.profilePictureURL, 80, false),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(user.fullName()),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(user.email),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(user.userID),
-              ),
-            ],
-          ),
-        ),
+        body: const Center(child: Text('Will put stuff here soon')),
       ),
     );
   }
