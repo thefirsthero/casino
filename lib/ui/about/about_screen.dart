83ac5949ed9de100e5fa7741961bd408cd63c7c2
thefirsthero <kingvukosi@outlook.com>
2023-06-14 15:56:11 +0200

    about us additions

--- lib/ui/about/about_screen.dart
+++ lib/ui/about/about_screen.dart
@@ -32,7 +32,7 @@ class AboutPage extends StatelessWidget {
                 elevation: 3,
                 child: ExpansionTile(
                   title: Text(
-                    'The traditional lottery',
+                    'The Traditional Lottery',
                     style: TextStyle(
                       fontSize: 18,
                       color: textColor,
@@ -50,11 +50,14 @@ class AboutPage extends StatelessWidget {
                       ),
                     ),
                     SizedBox(height: 8),
-                    Text(
-                      'Your odds of winning are approximately 1 in 45,057,474',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: textColor,
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Your odds of winning are approximately 1 in 45,057,474',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: textColor,
+                        ),
                       ),
                     ),
                   ],
@@ -70,7 +73,7 @@ class AboutPage extends StatelessWidget {
                 elevation: 3,
                 child: ExpansionTile(
                   title: Text(
-                    "The Fairman's lottery",
+                    'Fairman\'s Lottery',
                     style: TextStyle(
                       fontSize: 18,
                       color: textColor,
@@ -80,7 +83,7 @@ class AboutPage extends StatelessWidget {
                     Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: Text(
-                        'Aims to not only increase your odds of winning but increase the fairness of the game, and that starts with giving you choice',
+                        "Aims to not only increase your odds of winning but also increase the fairness of the game by giving you choice. See the tile below for more information.",
                         style: TextStyle(
                           fontSize: 16,
                           color: textColor,
@@ -88,42 +91,78 @@ class AboutPage extends StatelessWidget {
                       ),
                     ),
                     SizedBox(height: 8),
-                    Text(
-                      'There are 3 draws that happen every week:',
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: textColor,
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                          'There are 3 draws that happen every week. On Tuesday, Thursday and Sunday, all at 21:00 SAST.',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: textColor,
+                          ),
+                        ),
+                    ),
+                    SizedBox(height: 8),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                          'You are able to enter a draw at any point between 1 hour after the last draw, that is 22:00 SAST, and 1 hour before the draw closes, that is 20:00 SAST.',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: textColor,
+                          ),
+                        ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 16),
+              Card(
+                color: Theme.of(context).cardColor,
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: textColor ?? Colors.black),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                elevation: 3,
+                child: ExpansionTile(
+                  title: Text(
+                    'Better Draw Mechanics',
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: textColor,
+                    ),
+                  ),
+                  children: [
+                    SizedBox(height: 8),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        '- Tuesday draw: Pick 4 numbers within the range 1 to 29. Odds of winning: approximately 1 in 16,290.',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: textColor,
+                        ),
                       ),
                     ),
+                    SizedBox(height: 8),
                     Padding(
-                      padding: const EdgeInsets.only(left: 16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            '- Tuesday draw: Pick 4 numbers within the range 1 to 29. Odds of winning: approximately 1 in 16,290.',
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: textColor,
-                            ),
-                          ),
-                          SizedBox(height: 8),
-                          Text(
-                            '- Thursday draw: Pick 3 numbers within the range 1 to 39. Odds of winning: approximately 1 in 9,139.',
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: textColor,
-                            ),
-                          ),
-                          SizedBox(height: 8),
-                          Text(
-                            '- Sunday draw: Pick 2 numbers within the range 1 to 49. Odds of winning: approximately 1 in 1,176.',
-                            style: TextStyle(
-                              fontSize: 16,
-                              color: textColor,
-                            ),
-                          ),
-                        ],
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        '- Thursday draw: Pick 3 numbers within the range 1 to 39. Odds of winning: approximately 1 in 9,139.',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: textColor,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 8),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        '- Sunday draw: Pick 2 numbers within the range 1 to 49. Odds of winning: approximately 1 in 1,176.',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: textColor,
+                        ),
                       ),
                     ),
                   ],
