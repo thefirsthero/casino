9a70a62be0487fb2df7aafa3af6e6616daac38f3
thefirsthero <kingvukosi@outlook.com>
2023-08-23 13:36:24 +0200

    test

--- lib/constants.dart
+++
@@ -1,7 +0,0 @@
-const finishedOnBoardingConst = 'finishedOnBoarding';
-// const colorPrimary = 0xFF4549BC;
-const colorPrimary = 0xFF00B0FF;
-const facebookButtonColor = 0xFF415893;
-const facebookAppID = '285315185217069';
-const usersCollection = 'users';
-const eula = 'https://www.instamobile.io/eula-instachatty/';
